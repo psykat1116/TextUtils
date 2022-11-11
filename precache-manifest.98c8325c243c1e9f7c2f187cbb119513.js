768838c141b9b3e40eabdef6890dd93610e7e2e9
Saikat Samanta <95569573+psykat1116@users.noreply.github.com>
2022-11-11 11:23:47 +0530

    Updates

---
+++ precache-manifest.98c8325c243c1e9f7c2f187cbb119513.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e3b1eae5284659beb5e",
+    "url": "/TextUtils/static/css/main.d8d2641a.chunk.css"
+  },
+  {
+    "revision": "5e3b1eae5284659beb5e",
+    "url": "/TextUtils/static/js/main.5e3b1eae.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/TextUtils/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/TextUtils/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "05b54db4a4c3678b5054",
+    "url": "/TextUtils/static/js/runtime~main.05b54db4.js"
+  },
+  {
+    "revision": "9458f485bc7f117384391d6f9e867e58",
+    "url": "/TextUtils/index.html"
+  }
+];
